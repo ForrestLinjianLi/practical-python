816bf51c3b8a171eb4ccdce2ef974f379ee6f753
lilinjian <lljsg123123@gmail.com>
2025-03-20 21:11:50 -0700

    added a :

--- Solutions/1_10/mortgage.py
+++ Solutions/1_10/mortgage.py
@@ -21,7 +21,7 @@ while principal > 0:
 
     print(month, round(total_paid,2), round(principal, 2))
     
-print('Total paid', round(total_paid, 2))
+print('Total paid: ', round(total_paid, 2))
 print('Months', month)
 
 
